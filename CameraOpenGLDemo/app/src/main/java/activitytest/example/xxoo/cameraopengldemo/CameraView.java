612faf66af93636e09ed5eee6f018ab436e1ce79
495794643@qq.com <495794643@qq.com>
2017-02-09 21:22:32 +0800

    openGL

--- CameraOpenGLDemo/app/src/main/java/activitytest/example/xxoo/cameraopengldemo/CameraView.java
+++ CameraOpenGLDemo/app/src/main/java/activitytest/example/xxoo/cameraopengldemo/CameraView.java
@@ -1,7 +1,11 @@
 package activitytest.example.xxoo.cameraopengldemo;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -10,18 +14,36 @@ import android.view.SurfaceView;
  */
 
 public class CameraView extends SurfaceView implements SurfaceHolder.Callback , Camera.PreviewCallback{
+
+    //源视频帧 宽/高
+    private int srcFrameWidth  = 640;
+    private int srcFrameHeight = 480;
+    //数据采集
+    private int curCameraIndex = 1;
+    private Camera camera = null;
+    //视频帧共享存储回调接口
+    private SurfaceHolder surfaceHolder;
+
     public CameraView(Context context) {
         super(context);
+        if (Camera.getNumberOfCameras() > 1){
+            curCameraIndex = 1;
+        }else {
+            curCameraIndex = 0;
+        }
+        surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
 
     @Override
     public void onPreviewFrame(byte[] data, Camera camera) {
-
+    
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-
+        stopCamera();
     }
 
     @Override
@@ -32,5 +54,32 @@ public class CameraView extends SurfaceView implements SurfaceHolder.Callback , 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
 
+    }
+
+    //打开摄像头
+    private void startCamera(int cameraIndex){
+        initCamera(cameraIndex);
+    }
+
+    //初始化摄像头
+    public void initCamera(int cameraIndex){
+        //初始化摄像头 并打开
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+
+        }else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA}, 1);//1 can be another integer
+        }
+    }
+
+    //停止并释放摄像头
+    public void stopCamera(){
+        if (camera != null){
+            camera.setPreviewCallback(null);
+            camera.stopPreview();
+            camera.release();
+            camera = null;
+        }
     }
 }
