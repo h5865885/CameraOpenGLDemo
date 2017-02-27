9aa64188dad94f02724653175acbee0da2e64e56
495794643@qq.com <495794643@qq.com>
2017-02-27 17:52:43 +0800

    唇形

--- CameraOpenGLDemo/app/src/main/java/lab/sodino/glsurface/FaceCanvasView.java
+++ CameraOpenGLDemo/app/src/main/java/lab/sodino/glsurface/FaceCanvasView.java
@@ -22,7 +22,9 @@ import android.widget.TextView;
 
 import com.megvii.facepp.sdk.Facepp;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 import activitytest.example.xxoo.cameraopengldemo.CGRect;
 import activitytest.example.xxoo.cameraopengldemo.R;
@@ -82,12 +84,14 @@ public class FaceCanvasView extends View {
     private Path upPath = new Path();
     private Path downPath = new Path();
 
-    private CGRect rect = new CGRect();
+    int[] upMousePoints   = {84, 85, 86, 87, 88, 89, 90, 100, 99, 98, 97, 96, 84};//13
+    int[] downMousePoints = {84, 96, 103, 102, 101, 100, 90, 91, 92, 93, 94, 95, 84};//13
+    private final int number = upMousePoints.length;
+    private PointF[] upPoints    = new PointF[number];
+    private PointF[] downPoints  = new PointF[number];
 
-    int[] upMousePoints = {84, 85, 86, 87, 88, 89, 90, 100, 99, 98, 97, 96, 84};
-    int[] downMousePoints = {84, 96, 103, 102, 101, 100, 90, 91, 92, 93, 94, 95, 84};
-    private PointF[] points =new PointF[upMousePoints.length];
-//    private PointF[]
+    private RectF    upRect     = new RectF();
+//    private RectF    downRect   = new RectF();
 
     private float transitionX(float x){
         return (480 - x) / 480 * Screen.mWidth;
@@ -100,75 +104,139 @@ public class FaceCanvasView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        mPaint.setColor(Color.argb(255, 150, 5, 210));//a:0-255
-        mPaint.setStrokeWidth(5);//画笔宽度
+        mPaint.setColor(Color.argb(0, 150, 5, 210));//a:0-255
+        mPaint.setStrokeWidth(0);//画笔宽度
         mPaint.setStyle(Paint.Style.STROKE);
         //画控制点
         upPath.reset();//reset path
+//        downPath.reset();
 
         if (facesPoints != null && facesPoints[0].points.length > 0) {
 
             for (int i = 0; i < upMousePoints.length; i++) {//上嘴唇
                 PointF pointf = facesPoints[0].points[upMousePoints[i]];
-                points[i].set(transitionX(pointf.x),transitionY(pointf.y));
+                if (upPoints[i] == null) {
+                    upPoints[i] = new PointF();
+                }
+                upPoints[i].set(transitionX(pointf.x), transitionY(pointf.y));
+//                canvas.drawPoint(points[i].x, points[i].y, mPaint);
             }
-//          {84, 85, 86, 87, 88, 89, 90, 100, 99, 98, 97, 96, 84};
-            upPath.moveTo(points[0].x,points[0].y);
-            upPath.cubicTo(points[85].x,points[85].y-5,points[86].x,points[86].y,points[87].x,points[87].y);
 
-//                canvas.drawPoint((480 - pointf.x) / 480 * Screen.mWidth, pointf.y / 640 * this.getHeight(), mPaint);
+            getBerzierpathControlPoints(upPoints);
+//            mPaint.setStrokeWidth(5);
+//            for (int i = 0;i<controlPoints.length;i++){
+//                PointF pointf = controlPoints[i];
+//                canvas.drawPoint(pointf.x,pointf.y, mPaint);
+//            }
 
+            upPath.moveTo(upPoints[0].x,upPoints[0].y);
+            for (int i = 0;i<number-1;i++){
+//                Log.d(TAG, "onDraw: i "+i);
+                int j = 2 * i -1 < 0?controlPoints.length-1:2*i-1;
+                int w = 2 * i < controlPoints.length? 2 * i:0;
+                int m = i + 1 < upPoints.length?i+1:0;
+//                Log.d(TAG, "onDraw: j="+j+"  w="+w+"  m="+m);
+                upPath.cubicTo(controlPoints[j].x,controlPoints[j].y,controlPoints[w].x,
+                        controlPoints[w].y,
+                        upPoints[m].x,upPoints[m].y);
+            }
             upPath.close();
             canvas.drawPath(upPath,mPaint);
 //            canvas.clipPath(upPath);
-//            canvas.translate(-rect.width,-rect.height);
-//            canvas.drawBitmap(bitmap,rect.x-rect.width-50,rect.y-50,null);
-//            canvas.translate(rect.width,rect.height);
-
+            mPaint.setColor(Color.argb(100, 150, 5, 210));//a:0-255
+//            upPath.computeBounds(upRect,false);
+//            canvas.drawBitmap(bitmap,upRect.left-50,upRect.top-50,mPaint);
 
 
             for (int i = 0; i < downMousePoints.length; i++) {//下嘴唇
                 PointF pointf = facesPoints[0].points[downMousePoints[i]];
-                canvas.drawPoint((480 - pointf.x) / 480 * Screen.mWidth, pointf.y / 640 * this.getHeight(), mPaint);
-//              Log.d(TAG, "onDraw: x = " + x + ",,y=" + y + "screen" + this.getHeight());
+                if (downPoints[i] == null) {
+                    downPoints[i] = new PointF();
+                }
+                downPoints[i].set(transitionX(pointf.x), transitionY(pointf.y));
+//                canvas.drawPoint(points[i].x, points[i].y, mPaint);
             }
+
+            getBerzierpathControlPoints(downPoints);
+//            mPaint.setStrokeWidth(5);
+//            for (int i = 0;i<controlPoints.length;i++){
+//                PointF pointf = controlPoints[i];
+//                canvas.drawPoint(pointf.x,pointf.y, mPaint);
+//            }
+            mPaint.setColor(Color.argb(0, 0, 255, 210));//a:0-255
+            upPath.moveTo(downPoints[0].x,downPoints[0].y);
+            for (int i = 0;i<number-1;i++){
+                int j = 2 * i -1 < 0?controlPoints.length-1:2*i-1;
+                int w = 2 * i < controlPoints.length? 2 * i:0;
+                int m = i + 1 < downPoints.length?i+1:0;
+                upPath.cubicTo(controlPoints[j].x,controlPoints[j].y,controlPoints[w].x,
+                        controlPoints[w].y,
+                        downPoints[m].x,downPoints[m].y);
+            }
+            upPath.close();
+            canvas.drawPath(upPath,mPaint);
+            canvas.clipPath(upPath);
+            mPaint.setColor(Color.argb(100, 150, 5, 210));//a:0-255
+            upPath.computeBounds(upRect,false);
+            canvas.drawBitmap(bitmap,upRect.left-50,upRect.top-50,mPaint);
         }
-//        Log.d(TAG, "onDraw: ");
-//
-//        //起点
-//        mPath.moveTo(100, 100);
-//        //mPath 控制点 x,y 终点 x,y
-//        mPath.quadTo(150, 0, 200, 200);
-//        //闭合
-//        //画path
-//        canvas.drawPath(mPath, mPaint);
-
-
-        // 创建画笔
-//        Paint p = new Paint();
-//        p.setColor(Color.RED);// 设置红色
-//
-//        canvas.drawText("画圆：", 10, 20, p);// 画文本
-//        canvas.drawCircle(60, 20, 10, p);// 小圆
 //        p.setAntiAlias(true);// 设置画笔的锯齿效果。 true是去除，大家一看效果就明白了
 //        canvas.drawCircle(120, 20, 20, p);// 大圆
+    }
 
-//        mPath.moveTo(300,300);
-//        mPath.quadTo(350,0,400,400);
-//        mPath.close();
+    PointF[] midPoints1     = new PointF[number];//都是13个
+    PointF[] midPoints2     = new PointF[number];
+    PointF[] distancePoints = new PointF[number];
+    PointF[] controlPoints  = new PointF[number * 2];
 
-
-
-//        canvas.drawPoint(480/2, 100, mPaint);
-//        画线
-//        canvas.drawLine(100,100,150,0,mPaint);
-//        canvas.drawLine(200,200,150,0,mPaint);
-//
-//        canvas.clipPath(mPath);
-//        canvas.drawBitmap(bitmap,0,0,null);
+    private PointF[] getBerzierpathControlPoints(PointF[] allPoints){
+        for (int i = 0;i < allPoints.length;i++){
+            if (midPoints1[i] == null){
+                midPoints1[i] = new PointF();
+            }
+            float x = (allPoints[i].x + allPoints[i+1<allPoints.length?i+1:0].x)/2;
+            float y = (allPoints[i].y + allPoints[i+1<allPoints.length?i+1:0].y)/2;
+            midPoints1[i].set(x,y);
+        }
+        for (int i = 0;i < midPoints1.length;i++){
+            if (midPoints2[i] == null){
+                midPoints2[i] = new PointF();
+            }
+            float x = (midPoints1[i].x + midPoints1[i+1<midPoints1.length?i+1:0].x)/2;
+            float y = (midPoints1[i].y + midPoints1[i+1<midPoints1.length?i+1:0].y)/2;
+            midPoints2[i].set(x,y);
+        }
+        for (int i = 0;i < midPoints2.length;i++){
+            if (distancePoints[i] == null){
+                distancePoints[i] = new PointF();
+            }
+            float x = (midPoints2[i].x - allPoints[i+1<midPoints2.length?i+1:0].x);
+            float y = (midPoints2[i].y - allPoints[i+1<midPoints2.length?i+1:0].y);
+            distancePoints[i].set(x,y);
+//            Log.d(TAG, "getBerzierpathControlPoints: i="+i+"  "+distancePoints[i]);
+        }
+        for (int i = 0;i < distancePoints.length;i++){
+            if (controlPoints[2*i] == null || controlPoints[2*i+1] ==null){
+                controlPoints[2*i] = new PointF();
+                controlPoints[2*i+1] = new PointF();
+            }
+            controlPoints[2*i].set(midPoints1[i].x - distancePoints[i].x,midPoints1[i].y - distancePoints[i].y);
+            int j = i+1<midPoints1.length?i+1:0;
+            controlPoints[2*i+1].set(midPoints1[j].x - distancePoints[i].x,midPoints1[j].y - distancePoints[i]
+                    .y);
+        }
+        return controlPoints;
     }
 
     public void onResumeConfig(Context context) {
-
+//            PointF point0 = new PointF(500,100);
+//            PointF point1 = new PointF(200,300);
+//            PointF point2 = new PointF(500,500);
+//            PointF point3 = new PointF(800,300);
+//
+//            points[0] = point0;
+//            points[1] = point1;
+//            points[2] = point2;
+//            points[3] = point3;
     }
 }
